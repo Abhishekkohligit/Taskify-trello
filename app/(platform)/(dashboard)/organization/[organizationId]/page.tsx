c87b2c9bf3414ebb32d391580d9da93fd39f4e14
Abhishek Kohli <ab02110211@gmail.com>
2023-12-23 08:11:14 +0000

    created server action webhooks for createing & serveractions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -4,7 +4,6 @@ import { db } from "@/lib/db";
 import { Board } from "./board";
 import { Form } from "./form";
 
-
 const OrganizationIdPage = async () => {
 	const boards = await db.board.findMany();
 
